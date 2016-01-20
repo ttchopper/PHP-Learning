42309f18548aad4b51f759d8a02f970a67b50683
andrew <twinklechopper@gmail.com>
2016-01-21 00:39:02 +0200

    Changed loop

--- million.php
+++ million.php
@@ -6,12 +6,13 @@ error_reporting(-1);
 $deposit = 10000;
 $percentPerYear = 1.1;
 $personCurrentAge = 16;
-for ($year = 1; $year <= $deposit; $year++) {
+for ($year = 0; $deposit < 1000000; $year++) {
     $deposit *= $percentPerYear;
-    if ($deposit >= 1000000) {
-        $personCurrentAge += $year;
-        echo "Million budet cherez $year\n"; 
-	echo "Clientu budet $personCurrentAge\n";
-	break;
-    }	
 }
+
+$personCurrentAge += $year;
+echo "Million budet cherez $year\n"; 
+echo "Clientu budet $personCurrentAge\n";
+    
+	
+
